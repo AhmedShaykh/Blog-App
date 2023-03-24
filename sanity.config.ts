309250f68a73e52f7ef608333c24ef0279b2705c
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-03-24 23:15:38 +0500

    Final Commit

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,6 +4,7 @@ import { visionTool } from '@sanity/vision';
 import { schemaTypes } from './schemas';
 import { myTheme } from './theme';
 import StudioNavbar from './Components/StudioNavbar';
+import { getDefaultDocumentNode } from './structure';
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
@@ -14,7 +15,11 @@ export default defineConfig({
   title: 'MY JAMSTACK BLOG APP',
   projectId,
   dataset,
-  plugins: [deskTool(), visionTool()],
+  plugins: [
+    deskTool({
+      defaultDocumentNode: getDefaultDocumentNode
+    }), visionTool()
+  ],
   schema: {
     types: schemaTypes,
   },
